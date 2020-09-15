c490bc207fd242c51a804ea4127d20c249430efa
Bram Klein <bklein1993@gmail.com>
2020-09-15 16:56:12 +0200

    weather app concept toegevoegd met werkende API

---
+++ weather_app/weather_app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import './App.css';
+import Weatherdisplay from "./components/Weatherdisplay";
+
+function App() {
+  return (
+    <div className="App">
+
+      <Weatherdisplay />
+    </div>
+  );
+}
+
+export default App;
