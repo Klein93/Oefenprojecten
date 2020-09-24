ca183fca9946f3890636e72c869b8d002fed2f64
Bram Klein <bklein1993@gmail.com>
2020-09-24 19:59:59 +0200

    styling toegevoegd

--- ip_adress_tracker/ip-address-tracker-master/api-client.js
+++ ip_adress_tracker/ip-address-tracker-master/api-client.js
@@ -1,7 +1,8 @@
 
 const findIPLocation = async function (IPadress) {
 
-    let url = `https://geo.ipify.org/api/v1?apiKey=at_iMkaiv9KzDcr3upnhBYNOQcIcECUA&ipAddress=${IPadress}`;
+    let api_key = ""
+    let url = `https://geo.ipify.org/api/v1?apiKey=${api_key}&ipAddress=${IPadress}`;
     try {
         let data = await fetch(url, { method: "GET" })
         return data.json();
--- ip_adress_tracker/ip-address-tracker-master/script.js
+++ ip_adress_tracker/ip-address-tracker-master/script.js
@@ -1,16 +1,20 @@
 const inputfield = document.getElementById("inputfield");
 const searchbutton = document.getElementById("searchbutton");
 const mapdiv = document.getElementById("map");
+const ipadressP = document.getElementById("ipadress");
+const locationP = document.getElementById("location");
+const timezoneP = document.getElementById("timezone");
+const ispP = document.getElementById("isp");
 
 
 const mymap = L.map(mapdiv).setView([37.4223, -122.085], 13);
-L.tileLayer(`https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoia2xlaW45MyIsImEiOiJja2ZkcHFvc2kwMHJlMnJwYXYza3d0ZnF1In0.Bqf0s2BK7RAjQUi4IDGVhA`, {
+L.tileLayer(`https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accestoken}`, {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox/streets-v11',
     tileSize: 512,
     zoomOffset: -1,
-    accessToken: 'pk.eyJ1Ijoia2xlaW45MyIsImEiOiJja2ZkcHFvc2kwMHJlMnJwYXYza3d0ZnF1In0.Bqf0s2BK7RAjQUi4IDGVhA'
+    accessToken: ""
 }).addTo(mymap);
 
 
@@ -19,13 +23,18 @@ const showLocOnMap = async function (IPadress) {
     let lat = locData.location.lat;
     let lng = locData.location.lng;
     mymap.setView([lat, lng], 13);
+    L.marker([lat, lng]).addTo(mymap);
     showData(locData.ip, locData.location.region, locData.location.timezone, locData.isp)
 
-}
+};
 
 const showData = function (ipadress, location, timezone, isp) {
-    console.log(ipadress + location + timezone + isp)
-}
+    ipadressP.innerHTML = ipadress;
+    locationP.innerHTML = location;
+    timezoneP.innerHTML = timezone;
+    ispP.innerHTML = isp;
+
+};
 
 
 
