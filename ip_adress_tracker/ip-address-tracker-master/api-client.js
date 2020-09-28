1e1538250561caed6ca768b75e158ac89477745b
Bram Klein <bklein1993@gmail.com>
2020-09-28 12:19:03 +0200

    responsive design iptracker gemaakt

--- ip_adress_tracker/ip-address-tracker-master/api-client.js
+++ ip_adress_tracker/ip-address-tracker-master/api-client.js
@@ -1,7 +1,7 @@
 
 const findIPLocation = async function (IPadress) {
 
-    let api_key = ""
+    let api_key = "at_iMkaiv9KzDcr3upnhBYNOQcIcECUA"
     let url = `https://geo.ipify.org/api/v1?apiKey=${api_key}&ipAddress=${IPadress}`;
     try {
         let data = await fetch(url, { method: "GET" })
